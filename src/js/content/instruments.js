13bb5b82cbb1dac0cb47d08680997935d6f0ccdb
nicross <nicross@users.noreply.github.com>
2026-02-06 22:46:35 -0600

    Implement instrument navigation, appraisal, quirk discovery

--- src/js/content/instruments.js
+++ src/js/content/instruments.js
@@ -1,46 +1,86 @@
 content.instruments = (() => {
-  const map = new Map()
+  const generated = new Map(),
+    states = new Map()
 
   const defaultState = {}
 
+  function generate(name) {
+    const srand = (seed) => engine.fn.srand('instrument', name, 'attribute', seed)()
+
+    const rarity = srand('rarity')
+
+    const instrument = {
+      name,
+      quirks: [],
+      rarity: engine.fn.chooseWeighted([
+        {label: 'Common', weight: 1},
+        {label: 'Uncommon', weight: 1},
+        {label: 'Rare', weight: 1},
+        {label: 'Legendary', weight: 1},
+      ], rarity).label,
+    }
+
+    instrument.quirks.push({
+      name: 'Common quirk'
+    })
+
+    instrument.quirks.push({
+      isRare: true,
+      name: 'Rare quirk',
+    })
+
+    return instrument
+  }
+
   return {
     add: function (name) {
-      map.set(name, {...defaultState})
+      states.set(name, {...defaultState})
+
       return this
     },
+    count: () => states.size,
     export: function () {
       const data = {}
 
-      for (const [name, {name: _name, ...state}] of map.entries()) {
+      for (const [name, {name: _name, ...state}] of states.entries()) {
         data[name] = state
       }
 
       return data
     },
     get: function (name) {
-      if (!map.has(name)) {
-        map.set({
-          name,
-          ...defaultState,
-        })
+      if (!generated.has(name)) {
+        generated.set(name, generate(name))
       }
 
-      return map.get(name)
+      if (!states.has(name)) {
+        states.set(name, {...defaultState})
+      }
+
+      const instrument = generated.get(name)
+      instrument.state = states.get(name)
+
+      return instrument
     },
+    has: (name) => states.has(name),
     import: function (data = {}) {
       for (const [name, state] of Object.entries(data)) {
-        map.set(name, {name, ...state})
+        states.set(name, {name, ...state})
       }
 
       return this
     },
-    names: () => [...map.values()],
+    names: () => [...states.keys()],
     remove: function (name) {
-      map.delete(name)
+      generated.delete(name)
+      states.delete(name)
+
       return this
     },
     reset: function () {
-      map.clear()
+      generated.clear()
+      states.clear()
+
       return this
     },
   }
--- src/js/content/rooms/gallery.js
+++ src/js/content/rooms/gallery.js
@@ -10,5 +10,120 @@ content.rooms.gallery = content.rooms.invent({
   transitions: {
     up: 'atrium',
   },
-  // ...
+  // Methods
+  getInstrument: function () {
+    return this.state.name && content.instruments.has(this.state.name)
+      ? content.instruments.get(this.state.name)
+      : undefined
+  },
+  getDescription: function () {
+    const instrument = this.getInstrument()
+
+    if (instrument) {
+      return instrument.state.scans > 0
+        ? `${instrument.rarity} instrument`
+        : 'Unappraised'
+    }
+
+    return 'Bereft of instruments'
+  },
+  getDescriptionModifier: function () {
+    return this.getInstrument()?.rarity || ''
+  },
+  getName: function () {
+    return this.getInstrument()?.name || 'The gallery'
+  },
+  isDiscovered: function () {
+    return (this.getInstrument()?.state.scans || 0) > 0
+  },
+  setInstrumentByName: function (name) {
+    this.state.name = name
+
+    return this
+  },
+  // Interaction
+  canInteract: function () {
+    const instrument = this.getInstrument()
+
+    return instrument
+      ? (instrument.state.scans || 0) < instrument.quirks.length + 1
+      : false
+  },
+  onInteract: function () {
+    const instrument = this.getInstrument()
+    instrument.state.scans = (instrument.state.scans || 0) + 1
+
+    // Initial scan
+    if (instrument.state.scans == 1) {
+      const message = []
+
+      if (instrument.quirks.length) {
+        message.push(`${instrument.quirks.length} quirk${instrument.quirks.length == 1 ? '' : 's'} detected`)
+      }
+
+      return message.join(', ')
+    }
+
+    return `${instrument.quirks[instrument.state.scans - 2].name} found`
+  },
+  // Attributes
+  getAttributeLabels: function () {
+    const instrument = this.getInstrument()
+
+    if (!instrument?.state.scans) {
+      return []
+    }
+
+    const attributes = []
+
+    for (const i in instrument.quirks) {
+      const quirk = instrument.quirks[i]
+
+      if (instrument.state.scans - 1 > i) {
+        attributes.push({
+          label: quirk.name,
+          modifiers: [quirk.isRare ? 'rare' : ''],
+        })
+      } else {
+        attributes.push({
+          label: 'Unexamined quirk',
+          modifiers: ['undiscovered'],
+        })
+      }
+    }
+
+    return attributes
+  },
+  // Movement
+  canMoveLeft: () => content.instruments.count() > 1,
+  canMoveRight: () => content.instruments.count() > 1,
+  moveLeft: function () {
+    const names = content.instruments.names()
+
+    this.setInstrumentByName(
+      names[
+        engine.fn.wrap(names.indexOf(this.state.name) - 1, 0, names.length)
+      ]
+    )
+
+    return this.move('left')
+  },
+  moveRight: function () {
+    const names = content.instruments.names()
+
+    this.setInstrumentByName(
+      names[
+        engine.fn.wrap(names.indexOf(this.state.name) + 1, 0, names.length)
+      ]
+    )
+
+    return this.move('right')
+  },
+  onEnter: function () {
+    if (!this.getInstrument() && content.instruments.count()) {
+      this.setInstrumentByName(
+        content.instruments.names()[0]
+      )
+    }
+  },
 })
